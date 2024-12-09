4aa8a81cdc5e3be04d09acaffb688cc4d64d2cce
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-12-09 09:17:21 +0200

    task-3

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,21 +1,20 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-function getStatus(req, res) {
-  return res.status(200).json({
-    redis: redisClient.isAlive(),
-    db: dbClient.isAlive(),
-  });
+class AppController {
+  static getStatus(req, res) {
+    return res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static async getStats(req, res) {
+    return res.status(200).json({
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    });
+  }
 }
 
-async function getStats(req, res) {
-  return res.status(200).json({
-    users: await dbClient.nbUsers(),
-    files: await dbClient.nbFiles(),
-  });
-}
-
-export default {
-  getStatus,
-  getStats,
-};
+export default AppController;
